558032eddbc16532dcf4e4f731bc86634da84704
Huawen Yu <hyu@fortinet.com>
2026-05-23 14:20:55 -0700

    refactor from init.lua into this my plugin config ..

--- lua/c-utils/init.lua
+++ lua/c-utils/init.lua
@@ -15,33 +15,6 @@ function M.setup()
     return (g.c_utils_prefer_dir ~= "") and g.c_utils_prefer_dir or "daemon/wad"
   end
 
-  vim.keymap.set('n', '<leader><leader>', function()
-    vim.fn["VimMotionPreview"]()
-  end, { silent = true, desc = "[tag] Preview Tag *" })
-
-  vim.keymap.set('v', '<leader><leader>', function()
-    vim.fn["VimMotionPreview"]()
-  end, { silent = true, desc = "[tag] Preview Tag *" })
-
-  local function prepare_grep(is_visual, dir, to_qf)
-    vim.cmd("let g:grepper = {}")
-    if is_visual == 1 then
-      vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>gv<Esc>", true, false, true), "nx", false)
-    end
-    local target_dir = ""
-    if dir == "prefer" then
-      target_dir = get_prefer_dir()
-    end
-    local cmd = vim.fn["utilgrep#Grep"](0, is_visual, target_dir, to_qf)
-    local prefix = is_visual == 1 and ":<C-u>" or ":"
-    local keys = vim.api.nvim_replace_termcodes(prefix .. cmd, true, false, true)
-    vim.api.nvim_feedkeys(keys, "n", false)
-  end
-
-  vim.keymap.set('n', '<leader>gg', function() prepare_grep(0, "prefer", 1) end, { desc = "[find] Search in *" })
-  vim.keymap.set('v', '<leader>gg', function() prepare_grep(1, "prefer", 1) end, { desc = "[find] Search selection in *" })
-  vim.keymap.set('n', ';gg',        function() prepare_grep(0, "", 1) end, { desc = "[find] Search all *" })
-  vim.keymap.set('v', ';gg',        function() prepare_grep(1, "", 1) end, { desc = "[find] Search all *" })
 end
 
 return M
